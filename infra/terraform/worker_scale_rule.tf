934f0612c47cb7106e8eb4f1e5ac22b538706412
jirumil <jeanhilanga08@gmail.com>
2026-07-15 12:19:01 +0800

    infra: safely lock down environment as read-only data source to prevent deletions

--- infra/terraform/worker_scale_rule.tf
+++ infra/terraform/worker_scale_rule.tf
@@ -18,7 +18,7 @@
 resource "azurerm_container_app" "worker" {
   name                         = "guardrail-worker"
   resource_group_name          = azurerm_resource_group.main.name
-  container_app_environment_id = azurerm_container_app_environment.main.id
+  container_app_environment_id = data.azurerm_container_app_environment.main.id
   revision_mode                = "Single"
 
   registry {
